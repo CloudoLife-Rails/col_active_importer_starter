9b45642cc9c6c3856e0b94b5d2e9b3d985a83eea
Benjamin CloudoLife <benjamin@cloudolife.com>
2023-01-12 22:12:30 +0800

    Fix #1

--- lib/col_active_importer_starter/lib.rb
+++ lib/col_active_importer_starter/lib.rb
@@ -41,16 +41,25 @@ module ColActiveImporterStarter
 
   class BaseImporter < ActiveImporter::Base
 
+    attr_reader :start_at, :end_at, :time_consuming
+
     def get_result_id_column_index
+      return -1 if params.blank?
+
       params[ResultIndex::ID]
     end
 
     def initialize_result_exporter
+      return if params.blank?
+
       exporter = RubyXL::Parser.parse(params[:file])
       exporter_sheet1 = exporter[0]
 
       header_index = @header_index - 1
       column_index = get_result_id_column_index
+
+      return if invalid_column_index?(column_index)
+
       exporter_sheet1.add_cell(header_index, (column_index += 1), ResultColumnName::ID)
       exporter_sheet1.add_cell(header_index, (column_index += 1), ResultColumnName::Message)
 
@@ -59,30 +68,39 @@ module ColActiveImporterStarter
     end
 
     def initialize_model_cache
+      return if params.blank?
+
       model_cache = params.try(:[], :model_cache) || {}
 
       @model_cache = model_cache
     end
 
     on :import_started do
+      # initialize_count
+
       initialize_result_exporter
       initialize_model_cache
+
+      start_time_consuming
     end
 
-    # on :row_skipped do
-    #   sheet1 = @sheet1
-    #
-    #   row_index = @row_index - @header_index
-    #   column_index = ColumnIndex::Imported
-    #   column_index += 1
-    #   sheet1[row_index, (column_index += 1)] = ResultColumnName::Skip
-    # end
+    on :row_skipped do
+      # sheet1 = @sheet1
+
+      # row_index = @row_index - @header_index
+      # column_index = ColumnIndex::Imported
+      # column_index += 1
+      # sheet1[row_index, (column_index += 1)] = ResultColumnName::Skip
+    end
 
     def handle_row_success
       exporter_sheet1 = @exporter_sheet1
 
       row_index = @row_index - @header_index
       column_index = get_result_id_column_index
+
+      return if invalid_column_index?(column_index)
+
       column_index += 1
       exporter_sheet1.add_cell(row_index, (column_index += 1), ResultValue::Success)
     end
@@ -96,6 +114,9 @@ module ColActiveImporterStarter
 
       row_index = @row_index - @header_index
       column_index = get_result_id_column_index
+
+      return if invalid_column_index?(column_index)
+
       column_index += 1
       exporter_sheet1.add_cell(row_index, (column_index += 1), ResultValue::Fail)
       exporter_sheet1.add_cell(row_index, (column_index += 1), e.message)
@@ -110,6 +131,9 @@ module ColActiveImporterStarter
 
       row_index = @row_index - @header_index
       column_index = get_result_id_column_index
+
+      return if invalid_column_index?(column_index)
+
       exporter_sheet1.add_cell(row_index, (column_index += 1), model&.id)
     end
 
@@ -117,9 +141,10 @@ module ColActiveImporterStarter
       handle_row_processed
     end
 
-
     def handle_import_finished
       exporter = @exporter
+
+      return if params.blank?
 
       file = params[:file]
 
@@ -136,6 +161,36 @@ module ColActiveImporterStarter
 
     on :import_finished do
       handle_import_finished
+
+      end_start_time_consuming
+    end
+
+    def info
+      {
+        start_at: start_at,
+        end_at: end_at,
+        time_consuming: time_consuming,
+
+        row_count: row_count,
+        row_processed_count: row_processed_count,
+        row_success_count: row_success_count,
+        row_error_count: row_error_count,
+      }
+    end
+
+    private
+
+    def invalid_column_index?(column_index)
+      column_index == -1
+    end
+
+    def start_time_consuming
+      @start_at = Time.now
+    end
+
+    def end_start_time_consuming
+      @end_at = Time.now
+      @time_consuming = @end_at - @start_at
     end
   end
 end
